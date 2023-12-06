1151a28f401399195fbb05c13b992f7e2ad08115
vikram bhadoriya <vikrambhadoriya90@gmail.com>
2023-12-07 00:35:38 +0530

    commit for vercel

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,16 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'https://blogapp-l1r9.onrender.com',
+      "/api": {
+        target: "https://blogapp-l1r9.onrender.com",
         changeOrigin: true,
-      
+        secure: false,
       },
     },
   },
-})
+});
